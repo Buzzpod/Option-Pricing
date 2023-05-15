fc080a574137954e8c789e94cb60b710797be8b6
zain-kakujee <zainkakujee@kubrickgroup.com>
2023-05-15 19:36:37 +0100

    adding comments to AsianOption.hpp file and asian option tests

--- src/AsianOption.hpp
+++ src/AsianOption.hpp
@@ -3,13 +3,17 @@
 
 #include "Option.hpp"
 
+// Defines class called AsianOption derived from Option class
 class AsianOption : public Option {
 public:
     enum class AveragingType { Arithmetic, Geometric };
 
+    // Specifying constructor with three member variables from base Option class and initialises additional member variables specific to AsianOption
     AsianOption(double strike, double expiry, Option::Type optionType, AveragingType averagingType, unsigned int averagingPeriods);
 
-    double payoff(double averagePrice) const override;
+    double payoff(double averagePrice) const override; // Overrides payoff() method inherited from the base Option class
+
+    // Accessor functions to retrieve values of the member variables
     AveragingType getAveragingType() const;
     unsigned int getAveragingPeriods() const;
 
--- tests/test_asian_option.cpp
+++ tests/test_asian_option.cpp
@@ -1,3 +1,55 @@
-//
-// Created by George on 12/05/2023.
-//
+#include <gtest/gtest.h>
+#include "../src/Option.hpp"
+#include "../src/AsianOption.hpp"
+
+// Test fixture class for AsianOption testing
+class AsianOptionTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Initialize objects for the test
+        double strike_price = 105.0;
+        double expiry_time = 1.0;
+        AsianOption::AveragingType averagingType = AsianOption::AveragingType::Arithmetic;
+        unsigned int averagingPeriods = 10;
+        asianOption = new AsianOption(strike_price, expiry_time, Option::Type::Call, averagingType, averagingPeriods);
+    }
+
+    // Free any resources that were allocated for the test
+    void TearDown() override {
+        delete asianOption;
+    }
+
+    // Pointer to the AsianOption object for testing
+    AsianOption* asianOption;
+};
+
+// Test case for getAveragingType() function
+TEST_F(AsianOptionTest, GetAveragingType) {
+    AsianOption::AveragingType expectedAveragingType = AsianOption::AveragingType::Arithmetic;
+    EXPECT_EQ(expectedAveragingType, asianOption->getAveragingType());
+}
+
+// Test case for getAveragingPeriods() function
+TEST_F(AsianOptionTest, GetAveragingPeriods) {
+    unsigned int expectedAveragingPeriods = 10;
+    EXPECT_EQ(expectedAveragingPeriods, asianOption->getAveragingPeriods());
+}
+
+// Test case for getStrike() function (inherited from Option class)
+TEST_F(AsianOptionTest, GetStrike) {
+    double expectedStrike = 105.0;
+    EXPECT_EQ(expectedStrike, asianOption->getStrike());
+}
+
+// Test case for getExpiry() function (inherited from Option class)
+TEST_F(AsianOptionTest, GetExpiry) {
+    double expectedExpiry = 1.0;
+    EXPECT_EQ(expectedExpiry, asianOption->getExpiry());
+}
+
+// Test case for getType() function (inherited from Option class)
+TEST_F(AsianOptionTest, GetType) {
+    Option::Type expectedType = Option::Type::Call;
+    EXPECT_EQ(expectedType, asianOption->getType());
+}
+
