96cbe3a8b8f3c0058039002c5b39bca16e8417f3
Denis Maruev <86889764+RealSirScrubbington@users.noreply.github.com>
2023-05-22 09:49:26 +0100

    Update PricingEngine.hpp

--- src/PricingEngine.hpp
+++ src/PricingEngine.hpp
@@ -7,5 +7,6 @@ public:
     static double calculatePriceNaive(const Option &option, double spot, double riskFreeRate, double volatility, unsigned int numSimulations);
     static double calculatePriceAntithetic(const Option &option, double spot, double riskFreeRate, double volatility, unsigned int numSimulations);
     static double calculatePriceGBM(const Option& option, double spot, double riskFreeRate, double volatility, unsigned int numSimulations);
+    static double SDE_control_variate_2(const Option& option, double S0, double K, double T, double r, double sigma_0, int Npaths);
 };
 
